2660fb7f3e3b37ec81582516fb3e07529697e6a6
ElenaCKay <elenakay1@googlemail.com>
2023-03-27 14:32:32 +0100

    Started writing the review Id tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,3 +30,34 @@ describe("GET /api/categories", () => {
             });
     });
 });
+
+describe.only("GET /api/reviews/:review_id", () => {
+    test("200: Responds with an object with the corresponding review_id", () => {
+        return request(app)
+            .get("/api/reviews?review_id=1")
+            .expect(200)
+            .then(({ body }) => {
+                const { reviews } = body;
+                expect(reviews).toHaveLength(1);
+                reviews.forEach((review) => {
+                    expect.objectContaining({
+                        title: expect.any(String),
+                        review_body: expect.any(String),
+                        desginer: expect.any(String),
+                        review_img_url: expect.any(String),
+                        votes: expect.any(Number),
+                        category: expect.any(String),
+                        owner: expect.any(String),
+                    });
+                });
+            });
+    });
+    test("400: responds with a bad request for an invalid category ID", () => {
+        return request(app)
+            .get("/api/reviews?review_id=not-a-num")
+            .expect(400)
+            .then(({ body }) => {
+                expect(body.msg).toBe("Invalid ID");
+            });
+    });
+});
