e1a12c669a42befab7162f53aae25316193c5b6d
ElenaCKay <elenakay1@googlemail.com>
2023-03-28 12:33:35 +0100

    refactored /* and get /api

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const app = express();
-const { getCategories } = require("./controllers/categories.controller");
+const { getCategories, errNotFound, serverRunning } = require("./controllers/categories.controller");
 const { getReviewById } = require("./controllers/reviews.controller");
 const { handlePSQL400s, handleCustomErrors, handle500Statuses } = require("./error_handling");
 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.status(200).send({ msg: "Server is up and running" });
-});
+app.get("/api", serverRunning);
 
 app.get("/api/categories", getCategories);
 
 app.get("/api/reviews/:review_id", getReviewById);
 
-app.all("/*", (req, res) => {
-    res.status(404).send({ msg: "Not found" });
-});
+app.all("/*", errNotFound);
 
-app.use(handlePSQL400s)
-app.use(handleCustomErrors)
-app.use(handle500Statuses)
+app.use(handlePSQL400s);
+app.use(handleCustomErrors);
+app.use(handle500Statuses);
 module.exports = app;
- 
--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -9,3 +9,11 @@ exports.getCategories = (req, res, next) => {
             next(err);
         });
 };
+
+exports.errNotFound = (req, res) => {
+    res.status(404).send({ msg: "Not found" });
+};
+
+exports.serverRunning = (req, res) => {
+    res.status(200).send({ msg: "Server is up and running" });
+}
